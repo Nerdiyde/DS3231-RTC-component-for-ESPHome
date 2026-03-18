ec54bffe0190850db7116a6ce912035e1aed9dc1
Nerdiy.de <accounts@Nerdiy.de>
2026-03-18 21:09:53 +0100

    Fix implementation

--- components/ds3231/time.py
+++ components/ds3231/time.py
@@ -1,14 +1,16 @@
 import esphome.config_validation as cv
 import esphome.codegen as cg
 from esphome import automation
-from esphome.components import i2c, time, sensor
+from esphome.components import i2c, time
 from esphome.const import CONF_ID
+from esphome.components import sensor
 
 DEPENDENCIES = ["i2c"]
 ds3231_ns = cg.esphome_ns.namespace("ds3231")
 DS3231Component = ds3231_ns.class_("DS3231Component", time.RealTimeClock, i2c.I2CDevice)
 WriteAction = ds3231_ns.class_("WriteAction", automation.Action)
 ReadAction = ds3231_ns.class_("ReadAction", automation.Action)
+
 
 CONFIG_SCHEMA = time.TIME_SCHEMA.extend(
     {
@@ -21,6 +23,7 @@ CONFIG_SCHEMA = time.TIME_SCHEMA.extend(
         ),
     }
 ).extend(i2c.i2c_device_schema(0x68))
+
 
 @automation.register_action(
     "ds3231.write_time",
@@ -36,6 +39,7 @@ async def ds3231_write_time_to_code(config, action_id, template_arg, args):
     await cg.register_parented(var, config[CONF_ID])
     return var
 
+
 @automation.register_action(
     "ds3231.read_time",
     ReadAction,
@@ -49,6 +53,7 @@ async def ds3231_read_time_to_code(config, action_id, template_arg, args):
     var = cg.new_Pvariable(action_id, template_arg)
     await cg.register_parented(var, config[CONF_ID])
     return var
+
 
 async def to_code(config):
     var = cg.new_Pvariable(config[CONF_ID])
